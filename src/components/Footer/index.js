e5f5c90a6c75e3b6ee4f66c296cf7fa1dbdfe874
Seldon2 <emilio.oberheller@hotmail.com>
2022-02-09 08:29:13 +0100

    start footer, change favicon

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Footer, LogoContainer, NavigationContainer, ContactContainer } from './styles';
+
+const Footer = () => {
+  return (
+    <Footer>
+      <LogoContainer>
+      </LogoContainer>
+      <NavigationContainer>
+      </NavigationContainer>
+      <ContactContainer>
+      </ContactContainer>
+    </Footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Footer = styled.footer`
+
+`
+
+export const LogoContainer = styled.div`
+
+`
+
+export const NavigationContainer = styled.div`
+
+`
+
+export const ContactContainer = styled.div`
+
+`
